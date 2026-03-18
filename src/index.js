c0ae69149a08d7625a52dc731cfba3189fc59dcf
BartKor <bartkorzilius@gmail.com>
2026-03-18 16:34:01 +0000

    add executablePath to Puppeteer launch

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ app.post('/generate', async (req, res) => {
     try {
         browser = await puppeteer.launch({
             headless: 'new',
+            executablePath: '/usr/bin/google-chrome',
             args: [
                 '--no-sandbox', 
                 '--disable-setuid-sandbox',
